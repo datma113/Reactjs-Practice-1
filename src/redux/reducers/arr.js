da785fc17b8a1538b78b6a1f6c371f9c0721b8d6
datma <datma113112111@gmail.com>
2021-03-07 16:31:22 +0700

    add localStorage....

--- src/redux/reducers/arr.js
+++ src/redux/reducers/arr.js
@@ -1,13 +1,19 @@
 import * as types from '../constants/ActionTypes'
 
-let initState = ['A', 'B', 'C']
+let localData = localStorage.getItem('array')
+
+let initState = localData ? JSON.parse(localData) : []
 
 let reducer = (state = initState, action) => {
 
      if(action.type === types.ADD_ITEM) {
           let newState = [...state]
-          newState.push(action.item)
-          
+      
+               newState.push(action.item)
+               localStorage.setItem('array', JSON.stringify(newState))
+         
+          console.log(localStorage.getItem('array'))
+
           return newState
      }
      return state
