f0dbb56b73e3f773f99aa6280db96c8ebb6895d0
datma <datma113112111@gmail.com>
2021-03-14 09:42:00 +0700

    fix some bug from axios...

--- src/components/TestAPI.js
+++ src/components/TestAPI.js
@@ -1,31 +1,42 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const TestAPI = () => {
+    let url = "http://localhost:8080/Springmvc_linhkienmaytinh/api/nhanviens";
 
-     let url = 'http://localhost:8080/Springmvc_linhkienmaytinh/api/nhanviens'
-   
-     let data = []
-     useEffect(() => {
-        axios.get(url).then(res => {
-             data = (res.data)
-        })
-        .catch(err => {
-             console.log(err)
-        })
-    })
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        async function getData() {
+            axios
+                .get(url)
+                .then((res) => {
+                    setData(res.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+        getData();
+    }, []);
 
-    setTimeout(() => {
-         const dataMap = data.map( x => {
-              return <p>x.manhanvien</p>
-         })
-    }, 5000);
-     return (
-          <div className="container">
-               test api 
-           
-          </div>
-     )
-}
+    const dataMap = data.map((x, index) => {
+        return (
+            <div  key={index}>
+                 <hr></hr>
+                <p> mã nhân viên: {x.manhanvien}</p>
+                <p > họ đệm: {x.hodem}</p>
+                <p> tên: {x.ten}</p>
+                
+            </div>
+        );
+    });
 
-export default TestAPI
+    return (
+        <div className="container">
+            test api
+            {dataMap}
+        </div>
+    );
+};
+
+export default TestAPI;
